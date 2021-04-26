80afb7b9bd7a803c7bb28d965193d3b53af8ef48
KevinGuedes <kevinguedes@edu.unifor.br>
2021-04-26 11:12:17 -0300

    Correção nas mensagens de erro de validação do backend

--- FinancialControl.API/FinancialControl.API/Validators/CategoryValidator.cs
+++ FinancialControl.API/FinancialControl.API/Validators/CategoryValidator.cs
@@ -11,16 +11,16 @@ namespace FinancialControl.API.Validators
         {
             RuleFor(c => c.Name)
                 .NotNull().WithMessage("Enter category name")
-                .MinimumLength(1).WithMessage("Short category name. Provide a more meaningful name")
+                .MinimumLength(1).WithMessage("Category name too short. Provide a more meaningful name")
                 .MaximumLength(50).WithMessage("Category name too long");
 
             RuleFor(c => c.Icon)
                 .NotNull().WithMessage("Enter category icon")
-                .MinimumLength(1).WithMessage("Invalid icon code")
-                .MaximumLength(50).WithMessage("Icon code too long");
+                .MinimumLength(1).WithMessage("Icon code too short")
+                .MaximumLength(15).WithMessage("Icon code too long");
 
             RuleFor(c => c.TypeId)
-                .Must(type => type != 0).WithMessage("Invalid type");
+                .Must(type => type != 0).WithMessage("Choose a type");
         }
     }
 }
